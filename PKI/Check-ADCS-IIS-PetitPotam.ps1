c8ebf3e0f882ef30ac3fb8917c26434bb52d624d
Paulo da Silva <pasilva@microsoft.com>
2021-07-27 11:51:03 -0300

    Fix bugs

--- PKI/Check-ADCS-IIS-PetitPotam.ps1
+++ PKI/Check-ADCS-IIS-PetitPotam.ps1
@@ -3,6 +3,7 @@ Import-Module IISAdministration
 Import-Module WebAdministration
 $sites = (Get-ChildItem 'IIS:\Sites').collection.path
 $IISdir = "IIS:\Sites\Default Web Site"
+$Kerb = 0
 
 Write-Host "Server name: $($env:COMPUTERNAME)" -ForegroundColor Yellow
 foreach ($site in $sites) {
@@ -13,19 +14,37 @@ foreach ($site in $sites) {
     $authstr = $auth.providers.collection.Value
     # Add/Remove authentication providers
     if ($site -match "CES_Kerberos") {
-        if ($authstr -notmatch "Negotiate:Kerberos") {
-            Write-Host ""
+        # Check if there are no providers configured
+        if ($null -eq $authstr) {
+            Write-Host "No provider found"
             Write-Host "Adding Negotiate:Kerberos provider" -ForegroundColor Yellow
-            Add-WebConfigurationProperty -pspath 'MACHINE/WEBROOT/APPHOST/Default Web Site' -location $site -filter "system.webServer/security/authentication/windowsAuthentication/providers" -name "." -value @{value = 'Negotiate:Kerberos' }            
+            Add-WebConfigurationProperty -pspath 'MACHINE/WEBROOT/APPHOST/Default Web Site' -location $site -filter "system.webServer/security/authentication/windowsAuthentication/providers" -name "." -value @{value = 'Negotiate:Kerberos' }
+    
         }
-        Write-Host "Negotiate:Kerberos is already configured as authentication provider" -ForegroundColor Yellow
+        else {
+            foreach ($authstrs in $authstr) {
+                # Check if Kerberos is configured
+                if ($authstrs -match "Kerberos") {
+                    $Kerb = 1
+                    Write-Host "Negotiate:Kerberos is already configured as authentication provider" -ForegroundColor Yellow
+                    Write-Host ""
+                }
+            }
+            # If Kerberos was not found on provider list, add Kerberos
+            if ($Kerb -eq 0) {
+                Write-Host
+                Write-Host "Adding Negotiate:Kerberos provider" -ForegroundColor Yellow
+                Add-WebConfigurationProperty -pspath 'MACHINE/WEBROOT/APPHOST/Default Web Site' -location $site -filter "system.webServer/security/authentication/windowsAuthentication/providers" -name "." -value @{value = 'Negotiate:Kerberos' }
+            }
+        }
         Write-Host "Removing all others providers" -ForegroundColor Yellow
         Remove-WebConfigurationProperty  -pspath 'MACHINE/WEBROOT/APPHOST/Default Web Site' -location $site -filter "system.webServer/security/authentication/windowsAuthentication/providers" -name "." -AtElement @{value = 'Negotiate' }
         Remove-WebConfigurationProperty  -pspath 'MACHINE/WEBROOT/APPHOST/Default Web Site' -location $site -filter "system.webServer/security/authentication/windowsAuthentication/providers" -name "." -AtElement @{value = 'NTLM' }
         Remove-WebConfigurationProperty  -pspath 'MACHINE/WEBROOT/APPHOST/Default Web Site' -location $site -filter "system.webServer/security/authentication/windowsAuthentication/providers" -name "." -AtElement @{value = 'Negotiate:PKU2U' }
         Remove-WebConfigurationProperty  -pspath 'MACHINE/WEBROOT/APPHOST/Default Web Site' -location $site -filter "system.webServer/security/authentication/windowsAuthentication/providers" -name "." -AtElement @{value = 'Negotiate:CloudAP' }
+        $auth = Get-WebConfiguration -filter /system.webServer/security/authentication/windowsAuthentication -PSPath "$IISdir\$($site)"
         Write-Host ""
-        Write-Host "Authentication providers after change" -ForegroundColor Yellow
+        Write-Host "Authentication providers after change" -ForegroundColor Cyan
         Write-Host "Site name: $($site)" -ForegroundColor Green
         Write-Host "Auth: " $auth.providers.collection.Value -ForegroundColor Green
     }
