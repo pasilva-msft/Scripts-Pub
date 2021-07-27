b526906520b53cd46e62eb7338c99792ead4b9a8
Paulo da Silva <pasilva@microsoft.com>
2021-07-27 12:30:35 -0300

    Add script description

--- PKI/Check-ADCS-IIS-PetitPotam.ps1
+++ PKI/Check-ADCS-IIS-PetitPotam.ps1
@@ -1,3 +1,26 @@
+# This Sample Code is provided for the purpose of illustration only
+# and is not intended to be used in a production environment.  THIS
+# SAMPLE CODE AND ANY RELATED INFORMATION ARE PROVIDED "AS IS" WITHOUT
+# WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT
+# LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS
+# FOR A PARTICULAR PURPOSE.  We grant You a nonexclusive, royalty-free
+# right to use and modify the Sample Code and to reproduce and distribute
+# the object code form of the Sample Code, provided that You agree:
+# (i) to not use Our name, logo, or trademarks to market Your software
+# product in which the Sample Code is embedded; (ii) to include a valid
+# copyright notice on Your software product in which the Sample Code is
+# embedded; and (iii) to indemnify, hold harmless, and defend Us and
+# Our suppliers from and against any claims or lawsuits, including
+# attorneys'' fees, that arise or result from the use or distribution
+# of the Sample Code.
+#
+#   Author: Paulo da Silva
+#
+#   Script to configure IIS CES application (Certificate Enrollment Web Services)
+#   to use Negotiate:Kerberos and mitigate PetitPotam
+#
+
+
 # Query IIS configuration
 Import-Module IISAdministration
 Import-Module WebAdministration
@@ -16,7 +39,7 @@ foreach ($site in $sites) {
     if ($site -match "CES_Kerberos") {
         # Check if there are no providers configured
         if ($null -eq $authstr) {
-            Write-Host "No provider found"
+            Write-Host "No provider found" -ForegroundColor Red
             Write-Host "Adding Negotiate:Kerberos provider" -ForegroundColor Yellow
             Add-WebConfigurationProperty -pspath 'MACHINE/WEBROOT/APPHOST/Default Web Site' -location $site -filter "system.webServer/security/authentication/windowsAuthentication/providers" -name "." -value @{value = 'Negotiate:Kerberos' }
     
